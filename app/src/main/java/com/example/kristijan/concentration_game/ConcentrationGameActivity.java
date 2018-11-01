b15449273dbfbdabd2168f086b5278348d94731b
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2018-11-01 00:54:41 +0000

    first commit

---
+++ app/src/main/java/com/example/kristijan/concentration_game/ConcentrationGameActivity.java
@@ -0,0 +1,13 @@
+package com.example.kristijan.concentration_game;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConcentrationGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_concentration_game);
+    }
+}
